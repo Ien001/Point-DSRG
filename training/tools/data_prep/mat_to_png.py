fd14b16f6adcf2ab3c2ccfc37e8a6176cf554cc0
mcever <austinmcever@gmail.com>
2019-02-04 15:21:34 -0800

    Updated mat_to_png.py

--- training/tools/data_prep/mat_to_png.py
+++ training/tools/data_prep/mat_to_png.py
@@ -3,6 +3,7 @@ import os
 import scipy
 from scipy.io import loadmat
 import numpy as np
+import cv2
 import pdb
 
 cls_dir = '/media/ssd1/austin/datasets/SBD/benchmark_RELEASE/dataset/cls'
@@ -12,19 +13,15 @@ for f in os.listdir(cls_dir):
     # open the .mat file
     mat_path = os.path.join(cls_dir, f)
     mat_obj = loadmat(mat_path)
-    arr_comp_mats = mat_obj['GTcls'][0][0][0]
-    assert(arr_comp_mats.shape == (20, 1))
-    img = np.zeros(mat_obj['GTcls'][0][0][0][0][0].shape).astype(np.uint8)
-    # for each class, there is a compressed matrix 
-    for i, comp_mat_arr in enumerate(arr_comp_mats):
-        assert(comp_mat_arr.shape == (1,))
-        comp_mat = comp_mat_arr[0]
-        if np.all(comp_mat == np.zeros(comp_mat.shape)):
-            continue
-        arr = comp_mat.toarray()
-        img[np.where(arr != 0)] = i+1 # replace 1s with the class number
+    img = mat_obj['GTcls']['Segmentation'][0][0]
     assert(not np.all(img == np.zeros(img.shape)))
     img_id = f[:-4] # remove .mat ext
+    
+    # new_img = img * 12
+    # cv2.imshow(img_id, new_img)
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
+
     scipy.misc.imsave(os.path.join(out_dir, img_id + '.png'), img)
     print(img_id)
     
